27586cc22cff85f2d70926d0f34a9caef6842667
CoffeeBjoern <merilgwen@gmail.com>
2024-01-02 11:14:49 +0100

    Backend -  OAuth 2 Client Credentials mit JWTs
    Installation von Axios im Backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,14 @@ const port = 8080;
 const filename = __dirname + "/weight.json";
 const filename2 = __dirname + "/data.json";
 
+const axios = require('axios');
+
+// Client Credentials
+const client_id = '5aee2cfe-1709-48a9-951d-eb48f8f73a74';
+const client_secret = '3309a57a-9214-40db-9abe-28b1bb30c08c';
+
+
+
 //Middleware
 app.use(express.json()); //for parsing application/json
 app.use(cors()); //for configuring Cross-Origin Resource Sharing (CORS)
@@ -14,6 +22,53 @@ function log(req, res, next) {
     next();
 }
 app.use(log);
+
+
+// Funktion zum Abrufen des Tokens
+//client_id und client_secret Konstanten werden an post übergeben, hier liegt schon der Fehler. Auch wenn Konstanten 
+//direkt in den Post geschrieben werden besteht der Fehler:
+//Fehler bei der Token-Erstellung: AxiosError {message: 'Request failed with status code 500', name: 'AxiosError' 
+async function getToken() {
+    try {
+      const response = await axios.post('https://rest.arbeitsagentur.de/oauth/gettoken_cc', {
+        client_id: client_id,
+        client_secret: client_secret,
+        grant_type: 'client_credentials'
+      });
+  
+      if (response.data && response.data.access_token) {
+        return response.data.access_token;
+      } else {
+        throw new Error('Token nicht erhalten');
+      }
+    } catch (error) {
+      throw new Error(`Fehler bei der Token-Erstellung: ${error.message}`);
+    }
+  }
+
+  //Abrufen der Daten
+  //Der "erhaltene Token" wird hier an den "get" weitergegeben und soll auf die Daten zugreifen 
+async function getDataFromToken() {
+    try {
+      const token = await getToken();
+  
+      const headers = {
+        'OAuthAccessToken': token
+      };
+  
+      const response = await axios.get('https://rest.arbeitsagentur.de/infosysbub/studisu/pc/v1/studienangebote', {
+        headers: headers
+      });
+  
+      console.log('API-Antwort:', response.data);
+    } catch (error) {
+      console.error('Fehler bei der API-Anfrage:', error.message);
+    }
+  }
+  
+  getDataFromToken();
+
+  
 
 
 //Endpoints
